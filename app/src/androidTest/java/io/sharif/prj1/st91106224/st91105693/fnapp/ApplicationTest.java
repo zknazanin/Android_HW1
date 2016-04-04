79d74f9333bc03c38ed323ed864be1450781151f
zknazanin <zk.nazanin@gmail.com>
2016-04-04 17:42:36 +0430

    hw1 maked

---
+++ app/src/androidTest/java/io/sharif/prj1/st91106224/st91105693/fnapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.sharif.prj1.st91106224.st91105693.fnapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
