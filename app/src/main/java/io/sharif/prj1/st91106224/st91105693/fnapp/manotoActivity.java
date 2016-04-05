4468c71c59fee208f4039d594c4e135a5b9ac79c
zknazanin <zk.nazanin@gmail.com>
2016-04-05 19:50:15 +0430

    arrow keys set

--- app/src/main/java/io/sharif/prj1/st91106224/st91105693/fnapp/manotoActivity.java
+++ app/src/main/java/io/sharif/prj1/st91106224/st91105693/fnapp/manotoActivity.java
@@ -8,29 +8,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class manotoActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manoto);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotation);
+        imageView = (ImageView) findViewById(R.id.myImage);
+        imageView.setAnimation(animation);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                imageView.setX(imageView.getX()+10);
+                return;
             }
         });
-    }
 
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this   items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_manoto, menu);
         return true;
     }
